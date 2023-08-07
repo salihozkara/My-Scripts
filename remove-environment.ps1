d4fb6824c25c712f01eb9ed1c31cbc4f9f6b940d
Salih <salihozkara00@gmail.com>
2023-08-07 14:58:37 +0300

    Update remove-environment.ps1

--- remove-environment.ps1
+++ remove-environment.ps1
@@ -9,13 +9,11 @@ $fullPath = (Get-Item -Path $path).FullName
 
 $envPath = [Environment]::GetEnvironmentVariable("PATH", "User")
 
-# Önce, yolu PATH listesinden tam olarak eşleşecek şekilde bulmak için düzenli ifadeyi hazırlayın.
 $regexPattern = [regex]::Escape(";{0}" -f $fullPath)
 $match = [regex]::Match($envPath, $regexPattern)
 
 if($match.Success)
 {
-    # Yolu bulduğumuzda, PATH listesinden çıkaralım.
     $envPath = $envPath.Remove($match.Index, $match.Length)
     [Environment]::SetEnvironmentVariable("PATH", $envPath, "User")
     Write-Host "Path removed from PATH variable."
